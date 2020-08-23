6030f43c0f63d1ba1cd3613d243b39dd974625b3
louiscklaw <louiscklaw@gmail.com>
2020-08-23 17:57:36 +0800

    update dot files,

--- warehouse.js
+++ warehouse.js
@@ -11,15 +11,15 @@ var db = Hubdb({
   branch: 'db'
   });
 
-db.list((err, a)=>{
-  console.log('listing,', a)
-})
+// db.list((err, a)=>{
+//   console.log('listing,', a)
+// })
 
 // db.get("4c30d8a1b5dcde897cc51aca2a8abfab.json",(err, contents)=>{
 //   console.log('get,', contents)
 // })
 
-// db.update("4c30d8a1b5dcde897cc51aca2a8abfab.json",{hello:'world'},(err, result, id)=>{
-//   console.log('result,', result)
-//   console.log('id,',id)
-// })
+db.update("helloworld_db.json",{hello:'world'},(err, result, id)=>{
+  console.log('result,', result)
+  console.log('id,',id)
+})
